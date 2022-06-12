179d648136ecacb98112c6a3e0432059a4ef5bb6
Maikel33 <perezguedesmaikel@gmail.com>
2022-06-12 11:31:07 -0400

    axios d emaravilla

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import axios from "axios";
 import './App.css';
 import Paginator from "./components/Paginador";
 function App() {
+    const[usuarios,setUsuarios]=useState([]);
+    const[tablausuario,setTablausuario]=useState([]);
+    const[busqueda,setBusqueda]=useState('');
+    const peticionget= async ()=>{
+       await axios.get('https://jsonplaceholder.typicode.com/users').then(response=>{
+            setUsuarios(response.data);
+        }).catch(error=>{
+            console.log(error);
+        });
+    }
+    useEffect(()=>{peticionget().then()},[]);
   return (
     <div className="App">
      <Paginator/>
