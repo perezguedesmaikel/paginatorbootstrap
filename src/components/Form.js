477eb77c14f0d28483ef15ac7e34d26231a37b20
Maikel33 <perezguedesmaikel@gmail.com>
2022-06-15 15:05:41 -0400

    formulario diseñado bien

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,25 @@ import React from "react";
 
 function Form() {
     return(
-        <div>MaikelPro</div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Formulario</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon">
+                        </span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <a className="navbar-brand" href="#">Envio de datos:</a>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="email" placeholder="email" aria-label="Search"/>
+                        <input className="form-control me-2" type="text" placeholder="name" aria-label="Search"/>
+                        <button className="btn btn-primary" type="submit">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
 
     )
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,12 @@
 import React,{Fragment} from "react";
 import Form from "./Form";
+import { BsFillBinocularsFill} from "react-icons/bs";
 
 function Navbar({hadleonselect,input,totaldatos}) {
     return(
         <Fragment>
             {/*Formulario navbar----------------------------------------------Formulario navbar*/}
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon">
-
-                        </span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                   data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr className="dropdown-divider"/>
-                                    </li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabIndex="-1"
-                                   aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
+           <Form/>
             {/*Formulario navbar----------------------------------------------Formulario navbar*/}
         <nav className="navbar navbar-expand-lg navbar-light bg-light bg-body">
             <div className="container-fluid">
@@ -60,10 +18,17 @@ function Navbar({hadleonselect,input,totaldatos}) {
                     <option value='25' className={`${totaldatos.length<25?"opsionoculta":''}`}>25</option>
                     <option value="50" className={`${totaldatos.length<50?"opsionoculta":''}`}>50</option>
                     <option value="100" className={`${totaldatos.length<100?"opsionoculta":''}`}>100</option>
-                </select><label className='m-2'>entries</label></form>
-                <form className="d-flex m-2 mb-1">
-                    <input className="form-control m-2" type="search" placeholder="Search" aria-label="Search" onChange={input}/>
+                </select><label className='m-2'>entries</label>
                 </form>
+                <form className="d-flex m-2 mb-1">
+
+                    <div className="input-group mb-3">
+                        <input type="search" className="form-control" placeholder="Search"
+                               aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={input}/>
+                        <span className="input-group-text" id="basic-addon2"><BsFillBinocularsFill/></span>
+                    </div>
+                </form>
+
 
             </div>
         </nav>
